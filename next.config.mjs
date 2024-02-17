ed7f3ce7c7e302f47d7633ea78822eb42206334d
gentarokai <kenny31999@gmail.com>
2024-02-17 10:58:58 +0900

    success on displaying content in en and ja

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+import createNextIntlPlugin from "next-intl/plugin";
+
+// set up the plugin which creates an alias to provide your i18n configuration
+const withNextIntl = createNextIntlPlugin();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
